bdd1ec2756a822df356fdfbbaefe22d1065dadc4
derwolfe <chriswwolfe@gmail.com>
2011-10-07 09:16:59 -0500

    added some of the function from double_list, will need to mess with header

--- Linked_List.cpp
+++ Linked_List.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 Linked_List::Linked_List()
 {
-
+  size = 0;
+  head = NULL;
+  tail = NULL; // add the tail
 }
 
 Linked_List::~Linked_List()
 {
-
+   while ( !is_empty ()) {
+    pop ();     
+  }
 }
 
 void Linked_List::print(ostream& os)
@@ -23,17 +27,45 @@ void Linked_List::print(ostream& os)
 }
 
 bool Linked_List::is_empty()
-{
-
+{  
+  if ( size == 0 ) {
+    //assert ( head == NULL );
+    return true;
+  } else {
+    return false;
+  }
 }
 
 int Linked_List::get_size()
 {
-
+  return size;
 }
 
 void Linked_List::insert(int index, Document* document)
 {
+  /* why using an index??
+   *
+   * Two cases (1) the list is empty, and (2) the list isn't empty
+   * CASE 1 - add initial node to the list, populate the item
+   */
+  Double_node *data_ptr  = new Double_node; 
+  data_ptr->item         = document;
+  if ( size == 0 ) {
+    head           = data_ptr;
+    tail           = data_ptr;
+    data_ptr->prev = NULL;
+    data_ptr->next = NULL;
+  /* 
+   * CASE 2 - add on to the end. The head pointer doesn't
+   * change because of the tail-addition.
+   */
+  } else if ( size > 0 ) {
+    data_ptr->prev  = tail;
+    tail->next      = data_ptr;
+    data_ptr->next  = NULL;
+    tail            = data_ptr;
+  }
+  size++;
 
 }
 
