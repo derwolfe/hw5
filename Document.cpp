3aab4f3d33e9ce185c0e4b7bb88512690bc76aa3
derwolfe <chriswwolfe@gmail.com>
2011-10-07 10:01:51 -0500

    check the switch statement, test next

--- Document.cpp
+++ Document.cpp
@@ -1,10 +1,19 @@
 #include "Document.h"
 
 using namespace std;
+/* default constructor that will set no elements, this
+ * was needed in the previous assignement"
+ */ 
+Document::Document()
+{
+  name = "";
+  priority = "";
+}
 
 Document::Document(string in_name, string in_priority)
 {
-
+  name = in_name;
+  priority = in_priority;
 }
 
 ostream& operator<<(ostream &os, Document &document)
@@ -15,20 +24,27 @@ ostream& operator<<(ostream &os, Document &document)
     
   return os;
 }
-
+/* priorities are enumerated -- switch statements?
+ */
 Priority Document::convert_priority(string str_priority)
 {
-
+  if (str_priority.compare("1") == 0) {
+    return PRIORITY_1;
+  } else if (str_priority.compare("2") == 0) {
+    return PRIORITY_2;
+  } else if (str_priority.compare("3") == 0) {
+    return PRIORITY_3;
+  }
 }
 
 Priority Document::get_priority()
 {
-
+  return priority;
 }
 
 string Document::get_name()
 {
-
+  return name;
 }
 
 
