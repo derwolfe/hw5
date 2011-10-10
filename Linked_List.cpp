ccbb487e5cdf6fc2e5a85faf1794c19f6ddf1290
derwolfe <chriswwolfe@gmail.com>
2011-10-09 21:18:36 -0500

    reimp with single LL + tail

--- Linked_List.cpp
+++ Linked_List.cpp
@@ -12,7 +12,7 @@ Linked_List::Linked_List()
 Linked_List::~Linked_List()
 {
    while ( !is_empty ()) {
-    pop ();     
+    remove (0);     
   }
 }
 
@@ -28,7 +28,7 @@ void Linked_List::print(ostream& os)
 
 bool Linked_List::is_empty()
 {  
-  if ( size == 0 ) {
+  if (size == 0) {
     //assert ( head == NULL );
     return true;
   } else {
@@ -43,48 +43,68 @@ int Linked_List::get_size()
 
 void Linked_List::insert(int index, Document* document)
 {
-  /* why using an index??
+  /* you should have a throw mechanism if the index is out of
+   * range, otherwise, all should work. 
    *
-   * Two cases (1) the list is empty, and (2) the list isn't empty
-   * CASE 1 - add initial node to the list, populate the item
+   * use a zero based index, i.e. head = 0.
    */
-  Double_node *data_ptr  = new Double_node; 
-  data_ptr->item         = document;
-  if ( size == 0 ) {
-    head           = data_ptr;
-    tail           = data_ptr;
-    data_ptr->prev = NULL;
-    data_ptr->next = NULL;
-  /* 
-   * CASE 2 - add on to the end. The head pointer doesn't
-   * change because of the tail-addition.
-   */
-  } else if ( size > 0 ) {
-    data_ptr->prev  = tail;
-    tail->next      = data_ptr;
-    data_ptr->next  = NULL;
-    tail            = data_ptr;
+  Node *node = new Node; 
+  node->doc   = document;
+  if (size == 0) {
+    head = node;
+    tail = node;
+    node->doc = doc;
+    node->next = NULL;
+    /* you could also try comparing size and index
+     * if ind >= size, tail; if ind < size, ind middle,
+     * if ind == 0, ins to head, but make sure about the overlap
+     * of ins to 0, w/ size == 0, and ins 0 w/ size > 0;
+     *
+     * tail should not need to be reset if adding to head
+     */
+  } else if ((size > 0) && (index == 0)) {
+    node->next = head;
+    head = node;
+  } else if ((size > 0) && (index < size )) {
+    /* find node at index - 1, set pointers
+     * insert to head has been handled, so this 
+     * case will not handle it
+     * this also ignores insert to tail, by checking if
+     * index < size. 
+     */
+    Node *target_prev = find (index - 1);
+    node->next = target->next;
+    target->next = node;
+    /* the tail pointer gives quick access to the node
+     */
+  } else if ((size > 0) && ( index >= size )) {
+    tail->next = node;
+    node->next = NULL;
+    tail = node;
   }
   size++;
-
 }
-
+/* find will be a utility function employed by remove
+ */
 Linked_List::Node* Linked_List::find(int index)
 {
 
 }
-
 void Linked_List::remove(int index)
-{
+{ 
 
 }
-
+/* find the item using FIND, then delete it
+ */
 bool Linked_List::remove(string name)
 {
 
 }
 
-
+/* public method that returns the actual data item
+ * contained in the node. Use find, take one step further
+ * and give the item contained in the node.
+ */
 Document* Linked_List::retrieve(int index)
 {
 
